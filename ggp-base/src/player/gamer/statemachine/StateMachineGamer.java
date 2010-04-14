e8a7339119b2db8d0e05c0a00f42c46c89eb7b32
evangtcox <evangtcox@716a755e-b13f-cedc-210d-596dafc6fb9b>
2010-04-14 23:41:24 +0000

    Comments added for javadoc
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@72 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/player/gamer/statemachine/StateMachineGamer.java
+++ ggp-base/src/player/gamer/statemachine/StateMachineGamer.java
@@ -16,26 +16,54 @@ import util.statemachine.exceptions.GoalDefinitionException;
 import util.statemachine.exceptions.MoveDefinitionException;
 import util.statemachine.exceptions.TransitionDefinitionException;
 
+/**
+ * The base class for Gamers that utilize state machines.  Any reasonable GGP player should subclass this class.
+ * See @SimpleSearchLightGamer, @HumanGamer, and @LegalGamer for examples.
+ * @author evancox
+ *
+ */
 public abstract class StateMachineGamer extends Gamer
 {
 	private MachineState currentState;
 	private Role role;
 	private StateMachine stateMachine;
+	
+	/**
+	 * Returns the current state
+	 * @return the current state
+	 */
 
 	public final MachineState getCurrentState()
 	{
 		return currentState;
 	}
+	
+	/**
+	 * Returns the current role
+	 * @return the current role
+	 */
 
 	public final Role getRole()
 	{
 		return role;
 	}
+	
+	/**
+	 * Returns the state machine.  This is used for calculating the next state and other operations such as computing
+	 * the legal moves for all players
+	 * 
+	 * @return a state machine
+	 */
 
 	public final StateMachine getStateMachine()
 	{
 		return stateMachine;
 	}
+	
+	/**
+	 * A wrapper function for stateMachineMetaGame.  Initializes the state machine and
+	 * role for the player using the match description, before calling stateMachineMetaGame
+	 */
 	
 	@Override
 	public final void metaGame(long timeout) throws MetaGamingException
@@ -55,6 +83,11 @@ public abstract class StateMachineGamer extends Gamer
 			throw new MetaGamingException();
 		}
 	}
+	
+	/**
+	 * A wrapper function to stateMachineSelectMove. Advances the state machine to the next state so that
+	 * stateMachineSelectMove can operate properly
+	 */
 
 	@Override
 	public final GdlSentence selectMove(long timeout) throws MoveSelectionException
@@ -83,11 +116,30 @@ public abstract class StateMachineGamer extends Gamer
 			throw new MoveSelectionException();
 		}
 	}
-
+	
+	/**
+	 * Defines the metagaming action taken by a player during the START_CLOCK
+	 * @param timeout the START_CLOCK for the current game
+	 * @throws TransitionDefinitionException
+	 * @throws MoveDefinitionException
+	 * @throws GoalDefinitionException
+	 */
+	
 	public abstract void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException;
-
+	
+	/**
+	 * Defines the algorithm that the player uses to select their move.
+	 * @param timeout the START_CLOCK for the current game
+	 * @return Move - the move selected by the player
+	 * @throws TransitionDefinitionException
+	 * @throws MoveDefinitionException
+	 * @throws GoalDefinitionException
+	 */
 	public abstract Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException;
 
+	/**
+	 * Cleans up the role, currentState and stateMachine.
+	 */
 	/* Helper methods for children */
     protected final void cleanupAfterMatch() {
         role = null;
@@ -96,6 +148,11 @@ public abstract class StateMachineGamer extends Gamer
         setMatch(null);
         setRoleName(null);
     }
+    
+    /**
+     * Switches stateMachine to newStateMachine
+     * @param newStateMachine the new state machine
+     */
     
     protected final void switchStateMachine(StateMachine newStateMachine) {
         try {        
