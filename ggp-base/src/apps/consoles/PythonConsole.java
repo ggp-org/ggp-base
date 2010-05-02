3d6ccdf039d2e0d09e44b70be4e9862a8a314c13
sam.schreiber@gmail.com <sam.schreiber@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2010-05-02 08:10:42 +0000

    Added the JythonConsole library for the Python GGP Console, and appropriate licenses for Jython and JythonConsole.
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@95 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/apps/consoles/PythonConsole.java
+++ ggp-base/src/apps/consoles/PythonConsole.java
@@ -1,6 +1,5 @@
 package apps.consoles;
 
-import java.util.Scanner;
 import org.python.util.PythonInterpreter;
 
 /**
@@ -8,44 +7,22 @@ import org.python.util.PythonInterpreter;
  * console that has full access to all of the Java classes in the project.
  * This allows you to quickly experiment with the classes, without having to
  * write a full-blown Java program.
+ *
+ * This uses the JythonConsole implementation of a console with all of the
+ * nice features like history, tab completion, et cetera. JythonConsole is an
+ * external open source project, accessible at:
  * 
- * TODO: This isn't a great implementation of a Python console. It would be
- *       excellent if there were a way to hook Idle, or another full-featured
- *       Python console implementation, into this so that it could access the
- *       GGP Base classes and also provide features like code completion, and
- *       hitting 'UP' to get previous lines, and so on.
+ *    http://code.google.com/p/jythonconsole/
+ *    
+ * The license for JythonConsole is available in the licenses/ directory.
  * 
  * @author Sam
  */
 public class PythonConsole {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         PythonInterpreter interpreter = new PythonInterpreter();
-        while(true) {
-            System.out.print(":> ");
-            String line = in.nextLine();
-            try {
-                System.out.println(interpreter.eval(line));
-            } catch(Exception e) {
-                try {
-                    interpreter.exec(line);
-                } catch(Exception e2) {
-                    if(e2.toString().startsWith("SyntaxError: (\"mismatched input '<EOF>' expecting INDENT\",")) {
-                        String inLine = in.nextLine();
-                        while(inLine.length() > 0) {
-                            line = line + '\n' + inLine;
-                            inLine = in.nextLine();
-                        }
-                        try {
-                            interpreter.exec(line);
-                        } catch(Exception e3) {
-                            System.err.println(e3);
-                        }
-                    } else {
-                        System.err.println(e2);
-                    }
-                }
-            }
-        }
+        interpreter.exec("import scripts");        
+        interpreter.exec("import external.JythonConsole.console");
+        interpreter.exec("external.JythonConsole.console.main(locals())");
     }
 }
