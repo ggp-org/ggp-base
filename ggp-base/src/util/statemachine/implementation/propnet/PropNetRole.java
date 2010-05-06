f8ef411a6af4c1051a8ff4859ccf2d1aa10534d5
evangtcox <evangtcox@716a755e-b13f-cedc-210d-596dafc6fb9b>
2010-05-06 23:55:41 +0000

    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@107 716a755e-b13f-cedc-210d-596dafc6fb9b

---
+++ ggp-base/src/util/statemachine/implementation/propnet/PropNetRole.java
@@ -0,0 +1,44 @@
+package util.statemachine.implementation.propnet;
+
+import util.gdl.grammar.GdlProposition;
+import util.statemachine.Role;
+import util.statemachine.implementation.prover.ProverRole;
+
+public class PropNetRole implements Role {
+	
+	private GdlProposition name;
+
+	public PropNetRole(GdlProposition name)
+	{
+		this.name = name;
+	}
+	@Override
+	public GdlProposition getName() {
+		return name;
+	}
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		if ((o != null) && (o instanceof PropNetRole))
+		{
+			PropNetRole role = (PropNetRole) o;
+			return role.name.equals(name);
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return name.hashCode();
+	}
+
+	@Override
+	public String toString()
+	{
+		return name.toString();
+	}
+
+}
