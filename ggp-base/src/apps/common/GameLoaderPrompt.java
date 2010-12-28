112448ce7d06f2ae627ed4688b7ef725186e1134
sam.schreiber@gmail.com <sam.schreiber@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2010-12-28 08:24:23 +0000

    Add a TODO discussing the direction I'd like to go with GameLoaderPrompt.
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@220 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/apps/common/GameLoaderPrompt.java
+++ ggp-base/src/apps/common/GameLoaderPrompt.java
@@ -15,6 +15,9 @@ public class GameLoaderPrompt {
         if (rval != JFileChooser.APPROVE_OPTION)
             return null;
 
+        // TODO: What if, in the future, the default repository is not the local repository?
+        // What we'd actually like is to display a list of games in the default repository,
+        // not a list of games pulled from the local hard drive.
         return GameRepository.getDefaultRepository().getGame(fileChooser.getSelectedFile().getName().replace(".kif", ""));        
     }
 }
