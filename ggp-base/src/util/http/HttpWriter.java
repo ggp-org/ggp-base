bb6ee01d2725707607ed623694bc85e63887202a
sam.schreiber@gmail.com <sam.schreiber@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2011-01-14 07:01:15 +0000

    Add a way to write to servers using GET rather than POST.
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@242 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/util/http/HttpWriter.java
+++ ggp-base/src/util/http/HttpWriter.java
@@ -5,9 +5,23 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.URLEncoder;
 
 public final class HttpWriter
 {
+    public static void writeAsClientGET(Socket socket, String data, String playerName) throws IOException
+    {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        PrintWriter pw = new PrintWriter(bw);
+
+        pw.println("GET /" + URLEncoder.encode(data, "UTF-8") + " HTTP/1.0");
+        pw.println("Accept: text/delim");
+        pw.println("Sender: GAMESERVER");
+        pw.println("Receiver: "+playerName);
+
+        pw.flush();
+    }
+    
 	public static void writeAsClient(Socket socket, String data, String playerName) throws IOException
 	{
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
