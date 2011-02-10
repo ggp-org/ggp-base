cc9d7b534a3c16ee89fc4470b307fa9f6d8c5f99
sam.schreiber@gmail.com <sam.schreiber@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2011-02-10 09:50:35 +0000

    More visualization cleanup.
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@283 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/apps/server/visualization/VizContainerPanel.java
+++ ggp-base/src/apps/server/visualization/VizContainerPanel.java
@@ -12,25 +12,22 @@ import javax.swing.JPanel;
 import util.xhtml.GameStateRenderPanel;
 
 @SuppressWarnings("serial")
-public class VizContainerPanel extends JPanel {
-	
-	public final String XML;
-	public final String XSL;
-	public VizContainerPanel(String XML, String XSL, VisualizationPanel parent)
+public class VizContainerPanel extends JPanel {	
+	public VizContainerPanel(String XML, String XSL, VisualizationPanel parent) 
 	{
-		this.XML = XML;
-		this.XSL = XSL;
 		Dimension d = GameStateRenderPanel.getDefaultSize();
-		this.setPreferredSize(d);
-		BufferedImage backimage = parent.getGraphicsConfiguration().createCompatibleImage(d.width,d.height);
+		setPreferredSize(d);
+		
+		BufferedImage backimage = parent.getGraphicsConfiguration().createCompatibleImage(d.width, d.height);
 		GameStateRenderPanel.renderImagefromGameXML(XML, XSL, backimage);
-		try
-		{
+		try {
 			ByteArrayOutputStream bos = new ByteArrayOutputStream();
 			ImageIO.write(backimage, "png", bos);
 			compressed = bos.toByteArray();
 			imageWritten = true;
-		} catch (Exception ex) {ex.printStackTrace();}
+		} catch (Exception ex) {
+		    ex.printStackTrace();
+		}
 	}
 	
 	private byte[] compressed = null;
@@ -44,7 +41,9 @@ public class VizContainerPanel extends JPanel {
 				BufferedImage img2;
 				img2 = ImageIO.read(new ByteArrayInputStream(compressed));
 				g.drawImage(img2, 0, 0, null);
-			} catch (Exception ex) {ex.printStackTrace();}
+			} catch (Exception ex) {
+			    ex.printStackTrace();
+			}
        }
 	}
 }
