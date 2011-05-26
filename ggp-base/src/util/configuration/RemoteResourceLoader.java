e644f4789840f938aef08e76a5983b66317df3e4
sam.schreiber@gmail.com <sam.schreiber@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2011-05-26 09:29:41 +0000

    Add functionality to the ResourceLoader to load resources via POST requests that have content. This is useful e.g. for issuing quick ping requests to players.
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@327 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/util/configuration/RemoteResourceLoader.java
+++ ggp-base/src/util/configuration/RemoteResourceLoader.java
@@ -3,6 +3,7 @@ package util.configuration;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -49,5 +50,24 @@ public class RemoteResourceLoader {
             theJSON.append(nextLine);
         } while (true);
         return theJSON.toString();
+    }
+
+    public static String postRawWithTimeout(String theURL, String toPost, int nTimeout) throws IOException {
+        URL url = new URL(theURL);
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.setDoOutput(true);
+        urlConnection.setConnectTimeout(nTimeout);
+        OutputStreamWriter out = new OutputStreamWriter(urlConnection.getOutputStream());
+        out.write(toPost);
+        out.close();
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        StringBuilder decodedString = new StringBuilder();
+        String decodedLine;
+        while ((decodedLine = in.readLine()) != null) {
+            decodedString.append(decodedLine);
+        }
+        in.close();
+        return decodedString.toString();
     }
 }
