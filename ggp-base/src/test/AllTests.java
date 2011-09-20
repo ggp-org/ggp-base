4394441daf0573571d337fd5820af57aaf892fd0
oporaca@gmail.com <oporaca@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2011-09-20 04:53:23 +0000

    Removing a comma that reportedly caused a compilation problem in some non-Eclipse compiler.
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@365 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/test/AllTests.java
+++ ggp-base/src/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ProverStateMachineTests.class,
-                     StaticValidationTests.class, 
+                     StaticValidationTests.class
                      })
 public class AllTests {
     
