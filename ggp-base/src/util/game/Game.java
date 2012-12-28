99d59f7d2bc0f0dfd1bf3f3e65605a7652612865
sam.schreiber@gmail.com <sam.schreiber@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2012-12-28 06:35:16 +0000

    Improvements to the rulesheet-to-Gdl conversion inside of the Game object.
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@469 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/util/game/Game.java
+++ ggp-base/src/util/game/Game.java
@@ -104,7 +104,28 @@ public final class Game {
      */
     public List<Gdl> getRules() {
     	try {
-	        List<Gdl> rules = new ArrayList<Gdl>();        
+    		// First, strip all of the comments from the rulesheet.
+    		StringBuilder rulesheetBuilder = new StringBuilder();
+    		String[] rulesheetLines = theRulesheet.split("\n");
+    		for (int i = 0; i < rulesheetLines.length; i++) {
+    			String line = rulesheetLines[i];
+    			int comment = line.indexOf(';');
+    			int cutoff = (comment == -1) ? line.length() : comment;
+    			rulesheetBuilder.append(line.substring(0, cutoff));
+    			rulesheetBuilder.append(" ");
+    		}
+    		String processedRulesheet = rulesheetBuilder.toString();
+    		
+    		// Also surround the rulesheet in parens to make it a proper
+    		// symbol list before we start processing it, if it's not already
+    		// formatted that way.
+    		if (!processedRulesheet.trim().startsWith("(")) {
+    			processedRulesheet = "( " + processedRulesheet + " )";
+    		}
+    		
+    		// Finally, process the rulesheet as a symbol list and construct
+    		// the Gdl objects based on it.
+	        List<Gdl> rules = new ArrayList<Gdl>();
 	        SymbolList list = (SymbolList) SymbolFactory.create(theRulesheet);
 	        for (int i = 0; i < list.size(); i++)
 	        {
