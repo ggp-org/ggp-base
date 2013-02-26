45990438d22dc15cdd62f85e541f929ca88eed6b
sam.schreiber@gmail.com <sam.schreiber@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2013-02-26 09:20:56 +0000

    Fix a bug in the example python gamer.
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@552 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src_py/random_gamer.py
+++ ggp-base/src_py/random_gamer.py
@@ -6,9 +6,7 @@ import random
 
 from org.ggp.base.util.statemachine import MachineState
 from org.ggp.base.util.statemachine.implementation.prover import ProverStateMachine
-
 from org.ggp.base.player.gamer.statemachine import StateMachineGamer
-from org.ggp.base.player.gamer.statemachine.reflex.event import ReflexMoveSelectionEvent
 
 class PythonRandomGamer(StateMachineGamer):
 
@@ -21,7 +19,6 @@ class PythonRandomGamer(StateMachineGamer):
     def stateMachineSelectMove(self, timeout):
         moves = self.getStateMachine().getLegalMoves(self.getCurrentState(), self.getRole())
         selection = random.choice(moves)
-        self.notifyObservers(ReflexMoveSelectionEvent(moves, selection, 1))
         return selection
         
     def stateMachineStop(self):
