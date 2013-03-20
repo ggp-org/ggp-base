fc8803e91bc70bd4f862586ea5c25103933bf8f4
sam.schreiber@gmail.com <sam.schreiber@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2013-03-20 08:01:22 +0000

    Clean up the presentation of the counter graph, and include a button to reset the time series.
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@597 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/org/ggp/base/player/gamer/statemachine/parametric/ParametricDetailPanel.java
+++ ggp-base/src/org/ggp/base/player/gamer/statemachine/parametric/ParametricDetailPanel.java
@@ -4,10 +4,13 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
@@ -58,25 +61,27 @@ public class ParametricDetailPanel extends DetailPanel {
         memTotal = new TimeSeries("Total Memory");        
         final TimeSeriesCollection memory = new TimeSeriesCollection();
         memory.addSeries(memUsage);
-        memory.addSeries(memTotal);        
+        memory.addSeries(memTotal);
         JFreeChart memChart = ChartFactory.createTimeSeriesChart(null, null, "Megabytes", memory, true, true, false);
-        memChart.setBackgroundPaint(getBackground());        
+        memChart.setBackgroundPaint(getBackground());
         ChartPanel memChartPanel = new ChartPanel(memChart);
-        memChartPanel.setPreferredSize(new Dimension(400, 200));
+        memChartPanel.setPreferredSize(new Dimension(500, 250));
         sidePanel.add(memChartPanel);
 
-        counters = new HashSet<Counter>();        
+        counters = new HashSet<Counter>();
         countersCollection = new TimeSeriesCollection();
         JFreeChart counterChart = ChartFactory.createTimeSeriesChart(null, null, "Count per 100ms", countersCollection, true, true, false);
+        counterChart.getXYPlot().getRangeAxis().setAutoRangeMinimumSize(1.0);
         counterChart.setBackgroundPaint(getBackground());        
         ChartPanel counterChartPanel = new ChartPanel(counterChart);
-        counterChartPanel.setPreferredSize(new Dimension(400, 200));
+        counterChartPanel.setPreferredSize(new Dimension(500, 250));
         sidePanel.add(counterChartPanel);
         
         new AddDataPointThread().start();		
 		
-		this.add(new JScrollPane(moveTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED), new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
-		this.add(sidePanel, new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
+		this.add(new JScrollPane(moveTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED), new GridBagConstraints(0, 0, 1, 2, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+		this.add(sidePanel, new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+		this.add(new JButton(resetButtonMethod()), new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.SOUTHEAST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
 	}
 
 	public void observe(Event event) {
@@ -125,6 +130,9 @@ public class ParametricDetailPanel extends DetailPanel {
 			series.add(new Millisecond(new Date()), value);
 			value = 0;
 		}
+		public void clear() {
+			series.clear();
+		}
 	}
 	
 	private class AddDataPointThread extends Thread {
@@ -143,5 +151,18 @@ public class ParametricDetailPanel extends DetailPanel {
 				}
 			}
 		}
+	}
+	
+	AbstractAction resetButtonMethod() {
+		return new AbstractAction("Reset Time Series") {
+			private static final long serialVersionUID = 1L;
+			public void actionPerformed(ActionEvent evt) {
+				memUsage.clear();
+				memTotal.clear();
+				for (Counter c : counters) {
+					c.clear();
+				}
+			}
+		};
 	}
 }
