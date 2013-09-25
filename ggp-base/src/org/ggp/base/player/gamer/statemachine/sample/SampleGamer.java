cfdaa87e7e1495a8f2d4f429454eb8b65c666750
bertrand.decoster@gmail.com <bertrand.decoster@gmail.com@716a755e-b13f-cedc-210d-596dafc6fb9b>
2013-09-25 23:07:03 +0000

    Comments for SampleGamer
    git-svn-id: http://ggp-base.googlecode.com/svn/trunk@686 716a755e-b13f-cedc-210d-596dafc6fb9b

--- ggp-base/src/org/ggp/base/player/gamer/statemachine/sample/SampleGamer.java
+++ ggp-base/src/org/ggp/base/player/gamer/statemachine/sample/SampleGamer.java
@@ -5,6 +5,7 @@ import org.ggp.base.apps.player.detail.SimpleDetailPanel;
 import org.ggp.base.player.gamer.exception.GamePreviewException;
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
@@ -15,24 +16,42 @@ import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 /**
  * SampleGamer is a simplified version of the StateMachineGamer, dropping some
  * advanced functionality so the example gamers can be presented concisely.
+ * This class implements 7 of the 8 core functions that need to be implemented
+ * for any gamer.
+ * 
+ * If you want to quickly create a gamer of your own, extend this class and
+ * add the last core function : public Move stateMachineSelectMove(long timeout)
  */
+
 public abstract class SampleGamer extends StateMachineGamer
 {
-	@Override
-	public String getName() {
-		return getClass().getSimpleName();
-	}
-	
-	@Override
-	public void preview(Game g, long timeout) throws GamePreviewException {
-		// Sample gamers do no game previewing.
-	}	
-	
 	@Override
 	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
 	{
 		// Sample gamers do no metagaming at the beginning of the match.
 	}	
+	
+	
+	
+	/** This will currently return "SampleGamer"
+	 * If you are working on : public abstract class MyGamer extends SampleGamer
+	 * Then this function would return "MyGamer"
+	 */
+	public String getName() {
+		return getClass().getSimpleName();
+	}
+	
+	// This is the default State Machine
+	public StateMachine getInitialStateMachine() {
+		return new CachedStateMachine(new ProverStateMachine());
+	}	
+
+	// This is the defaul Sample Panel
+	public DetailPanel getDetailPanel() {
+		return new SimpleDetailPanel();
+	}	
+	
+	
 	
 	@Override
 	public void stateMachineStop() {
@@ -45,12 +64,7 @@ public abstract class SampleGamer extends StateMachineGamer
 	}
 	
 	@Override
-	public StateMachine getInitialStateMachine() {
-		return new CachedStateMachine(new ProverStateMachine());
-	}	
-
-	@Override
-	public DetailPanel getDetailPanel() {
-		return new SimpleDetailPanel();
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// Sample gamers do no game previewing.
 	}	
 }
