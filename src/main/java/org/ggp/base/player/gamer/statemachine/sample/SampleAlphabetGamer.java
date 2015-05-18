8581470dc7506c830b8861102e07a6d7ac607142
samschreiber <sam.schreiber@gmail.com>
2015-05-17 22:02:31 -0700

    A long-standing problem with SampleLegalGamer is that its behavior depends on implementation details in how legal moves are determined. This can vary depending on the state machine being used, the way Java orders moves within certain data structures, etc. At best, this is merely confusing.
    That said, there is benefit to having the SampleLegalGamer as an example
    of a simple gamer that has reproducible behavior. This commit replaces
    the SampleLegalGamer with a new SampleAlphabetGamer, which returns the
    legal move which appears first in alphabetical order. This strategy is
    simple, deterministic, and preserves the benefits of SimpleLegalGamer
    while eliminating any inconsistencies due to state machines, data
    structure implementations, or lower-level implementation details.

--- src/main/java/org/ggp/base/player/gamer/statemachine/sample/SampleAlphabetGamer.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/sample/SampleAlphabetGamer.java
@@ -1,5 +1,7 @@
 package org.ggp.base.player.gamer.statemachine.sample;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
@@ -9,15 +11,16 @@ import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 /**
- * SampleLegalGamer is a minimal gamer which always plays the first
- * legal move it identifies, regardless of the state of the game.
+ * SampleAlphabetGamer is a minimal gamer which always plays the whichever
+ * legal move comes first in alphabetical order, regardless of the state of
+ * the game.
  *
  * For your first players, you should extend the class SampleGamer
  * The only function that you are required to override is :
  * public Move stateMachineSelectMove(long timeout)
  *
  */
-public final class SampleLegalGamer extends SampleGamer
+public final class SampleAlphabetGamer extends SampleGamer
 {
 	/**
 	 * This function is called at the start of each round
@@ -34,16 +37,23 @@ public final class SampleLegalGamer extends SampleGamer
 		/**
 		 * We put in memory the list of legal moves from the
 		 * current state. The goal of every stateMachineSelectMove()
-		 * is to return one of these moves. The choice of which
+		 * is to return one of these moves. Choosing the best
 		 * Move to play is the goal of GGP.
 		 */
 		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
 
-		// SampleLegalGamer is very simple : it picks the first legal move
+		// Sort the list of moves alphabetically.
+		Collections.sort(moves, new Comparator<Move>(){
+            @Override public int compare(Move m1, Move m2) {
+                return m1.toString().compareTo(m2.toString());
+            }
+        });
+
+		// Pick the move that comes first alphabetically.
 		Move selection = moves.get(0);
 
-		// We get the end time
-		// It is mandatory that stop<timeout
+		// Get the time when we finished.
+		// It is mandatory that "stop" be less than "timeout".
 		long stop = System.currentTimeMillis();
 
 		/**
