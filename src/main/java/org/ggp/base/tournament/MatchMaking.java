4728470161f852f6302ca9d2b3daf2e7fcabee60
utv <k9amata@gmail.com>
2015-08-20 00:21:40 -0700

    Matching by quality of players

--- src/main/java/org/ggp/base/tournament/MatchMaking.java
+++ src/main/java/org/ggp/base/tournament/MatchMaking.java
@@ -13,95 +13,91 @@ import org.bson.Document;
 import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.nio.file.Files;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import java.util.ArrayList;
+import java.util.*;
+
 import static java.util.Arrays.asList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-import java.io.IOException;
 import java.lang.IllegalStateException;
 import java.lang.RuntimeException;
 
+import org.ggp.base.player.GamePlayer;
+import org.ggp.base.player.gamer.Gamer;
+import org.ggp.base.server.GameServer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.game.GameRepository;
+import org.ggp.base.util.gdl.factory.exceptions.GdlFormatException;
+import org.ggp.base.util.match.Match;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.symbol.factory.exceptions.SymbolFormatException;
+
+import jskills.*;
+import jskills.trueskill.TwoPlayerTrueSkillCalculator;
+
+
 class MatchMaking {
 
-    private static final double INIT_SIGMA = 0.500;
-    private static final double INIT_MU = 0.500;
-    private static final double INIT_SKILL = 0.500;
+    public MatchMaking() {}
 
-    private static List<String> playersInTournament(MongoDatabase database, String tournament) {
-        // "_id" required by API usage
-        MongoCollection<Document> players = database.getCollection("players");
-        List<Document> playersInTournament = 
-            players.aggregate(
-                asList(
-                    new Document("$match", new Document("tournament", tournament)),
-                    new Document("$group", new Document("_id", "$username"))
-                    )).into(new ArrayList<Document>());
-
-        List<String> users = new ArrayList<String>();
-        for (Document aPlayer : playersInTournament)
-            users.add(aPlayer.get("_id").toString());
-        
-        return users;
-    }
-
-    private static List<String> latestRanksInTournament(MongoDatabase database, String tournament) {
-        MongoCollection<Document> matches = database.getCollection("matches");
-        Document latestMatch = matches.find(
-            eq("tournament", tournament)).sort(descending("createdAt")).first();
-
-        if (latestMatch == null) 
-            return playersInTournament(database, tournament);
-
-        List<Document> currentRanks = (List<Document>)latestMatch.get("currentRanks");
-        List<String> users = new ArrayList<String>();
-        for (Document aRank : currentRanks)
-            users.add(aRank.get("username").toString());
-        
-        return users;
-    }
-
-    public static void startMatch() {
-        
+    public static List<String> randomlyPickPlayers(List<String> avialableUsers, int numPlayers) {
+        long seed = System.nanoTime();
+        Collections.shuffle(avialableUsers, new Random(seed));
+        List<String> pickedUsers = new ArrayList<String>();
+        for (int i = 0; i < numPlayers; ++i ) 
+            pickedUsers.add(avialableUsers.remove(0));
+        return pickedUsers;
     }
 
     public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient( "localhost" , 3001 );
-        MongoDatabase database = mongoClient.getDatabase("meteor");
-        MongoCollection<Document> tournaments = database.getCollection("tournaments");
 
-        Document tour = tournaments.find().sort(descending("createdAt")).first();
-        System.out.println("===>  tournament = " + tour.get("name").toString());
-        
-        for (String aUser : latestRanksInTournament(database, tour.get("name").toString())) {
-            System.out.println("user = " + aUser);
+        TournamentManager tm = new TournamentManager("Tic");
+//        TournamentManager tm = new TournamentManager("FreeForAll");
+        long numberOfRandomMatches = 0;
+        long MAX_NUM_RANDOM_MATCHES = 5;
+
+        // Start loop
+        try {
+            while (true) {
+                System.out.println("Start loop....");
+                // pick random if no match yet.
+                int numPlayers = 2;
+                System.out.println("number of users in this tournament = " + tm.getAvailableUsers().size());
+                if (tm.getAvailableUsers().size() >= numPlayers) {
+
+                    if (MAX_NUM_RANDOM_MATCHES > numberOfRandomMatches || tm.numberOfMatches() < MAX_NUM_RANDOM_MATCHES) {
+                        System.out.println("Yes, We can play!");
+                        System.out.println("numberOfRandomMatches = " + numberOfRandomMatches);
+                        // choose players randomly for N matches
+                        System.out.println("Start match!");
+                        List<String> pickedUsers =
+                                randomlyPickPlayers(new ArrayList<String>(tm.getAvailableUsers()), tm.numberOfPlayers());
+                        tm.playOneVsOne(pickedUsers);
+                        numberOfRandomMatches++;
+
+                    } else {
+                        // get two least played user and both produce good match quality
+                        System.out.println("Yes, matchByQuality!");
+                        tm.matchByQuality();
+                    }
+                }
+                Thread.sleep(2 * 1000);
+            }
         }
-
-        String tourName = tour.get("name").toString();
-        List<String> users = latestRanksInTournament(database, tourName);
-         // = matchMaking(tour, users)
-        // while (true) {
-            // get users
-
-            // match users
-            // aMatch = {tour, [{username}]}
-            // remove playing users from users
-            // Document aMatch =  matchMaking(tour, users);
-
-            // play
-            // results = [{user, skill, sigma, mu, etc}] 
-            // Document results = startMatch(aMatch)
-
-            // update skills
-            // updateSkills(tour, users, results);
-            // push playedUser back into users
-        // }
-
+        catch (InterruptedException itre) {
+            itre.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 }
--- src/main/java/org/ggp/base/tournament/MongoTest.java
+++ src/main/java/org/ggp/base/tournament/MongoTest.java
@@ -9,20 +9,34 @@ import static com.mongodb.client.model.Sorts.ascending;
 import static com.mongodb.client.model.Filters.*;
 import org.bson.Document;
 
-import java.util.Arrays;
+import java.util.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-import org.apache.commons.io.FileUtils;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 import java.nio.file.Files;
-import java.util.Collection;
-import java.util.Iterator;
 
 import org.ggp.base.player.GamePlayer;
 import org.ggp.base.player.gamer.Gamer;
+import org.ggp.base.server.GameServer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.game.GameRepository;
+import org.ggp.base.util.gdl.factory.exceptions.GdlFormatException;
+import org.ggp.base.util.match.Match;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.symbol.factory.exceptions.SymbolFormatException;
+
+import jskills.*;
+import jskills.trueskill.TwoPlayerTrueSkillCalculator;
 
 class MongoTest {
 
@@ -72,79 +86,124 @@ class MongoTest {
         }
     }
 
-    public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient( "localhost" , 3001 );
-        MongoClient anotherMongoClient = new MongoClient( "localhost" , 3001 );
+    public static void playGame(List<String> hostNames, 
+                                List<String> playerNames, 
+                                List<Integer> portNumbers, 
+                                String gameKey,
+                                GamePlayer aGamePlayer) throws IOException, InterruptedException {
         
-        MongoDatabase database = mongoClient.getDatabase("meteor");
-        MongoDatabase anotherDatabase = anotherMongoClient.getDatabase("meteor");
+        Game game = GameRepository.getDefaultRepository().getGame(gameKey);
+        int expectedRoles = Role.computeRoles(game.getRules()).size();
+        if (hostNames.size() != expectedRoles) {
+            throw new RuntimeException("Invalid number of players for game " + gameKey + ": " + hostNames.size() + " vs " + expectedRoles);
+        }
 
-        // Document doc = new Document("name", "MongoDB")
-        //        .append("type", "database")
-        //        .append("count", 1)
-        //        .append("info", new Document("x", 203).append("y", 102))
-        //        .append("myArray", Arrays.asList(new Document("x", 203).append("y", 102),
-        //                                         new Document("x", 33).append("y", 222)));
-        // System.out.println("=================>  " + doc.toJson());
+        String tourneyName = "testPlayGame";
+        String matchName = tourneyName + "." + gameKey + "." + System.currentTimeMillis();
+        int startClock = 3;
+        int playClock = 14;
+        Match match = new Match(matchName, -1, startClock, playClock, game);
+        match.setPlayerNamesFromHost(playerNames);
 
-        // MongoCollection<Document> tournaments = database.getCollection("tournaments");
-        // Document tournament = tournaments.find(eq("name", "Money")).first();
+        // Actually run the match, using the desired configuration.
+        aGamePlayer.start();
+        GameServer server = new GameServer(match, hostNames, portNumbers);
+        server.start();
+        server.join();
+        aGamePlayer.shutdown();
+
+        // Open up the directory for this tournament.
+        // Create a "scores" file if none exists.
+        File f = new File(tourneyName);
+        if (!f.exists()) {
+            f.mkdir();
+            // f = new File(tourneyName + "/scores");
+            f.createNewFile();
+        }
+
+        // Open up the JSON file for this match, and save the match there.
+        f = new File(tourneyName + "/" + matchName + ".json");
+        if (f.exists()) f.delete();
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        bw.write(match.toJSON());
+        bw.flush();
+        bw.close();
         
-        // MongoCollection<Document> anotherTournaments = anotherDatabase.getCollection("tournaments");
-        // Document anotherTournament = tournaments.find(eq("name", "Money")).first();
-        
-        // System.out.println("Before Update: Money = " 
-        //     + tournament.get("game") + ", " + tournament.get("status"));
+    }
 
-        // tournaments.updateOne(eq("name", "Money"), 
-        //     new Document("$set", new Document("game", "Doo").append("status", "See")));
-
-        // System.out.println("After Update: Money = " + anotherTournament.get("game") + ", " + anotherTournament.get("status"));
-
-
-        MongoCollection<Document> players = database.getCollection("players");
-        Document aPlayer = players.find(eq("status", "compiled")).first();
+    public static GamePlayer gamePlayer(Document aPlayer) throws Exception {
         String pathToClasses = aPlayer.get("pathToClasses").toString();
+        URL url = new File(pathToClasses).toURL();
+        URL[] urls = new URL[]{url};
+        ClassLoader cl = new URLClassLoader(urls);
+        String[] extensions = {"class"};
+        String packageName = new File(pathToClasses).listFiles()[0].getName();
+        String pathToPackage = pathToClasses + "/" + packageName;
+        Collection<File> allClassFiles = 
+            FileUtils.listFiles(new File(pathToPackage), extensions, false);
+        
+        // Loop through all class files to find Gamer class.
+        for (Iterator<File> it = allClassFiles.iterator(); it.hasNext();) {
+            File f = it.next();
+            System.out.println("File name = " + f.getName());
+            String playerName = f.getName().split("\\.(?=[^\\.]+$)")[0];
+            String playerPackage = packageName + "." + playerName;
+            Class aClass = cl.loadClass(playerPackage);
+            
+            System.out.println("...........detecting gamer");
+            // found one and update player name, status and path to classes.
+            if (Gamer.class.isAssignableFrom(aClass)) {
+                System.out.println( f.getName() 
+                    + " is a player!!, and it is " + aClass.getSimpleName());
+                // Setup players
+                Gamer gamer = (Gamer) aClass.newInstance();
+                int port = 9147;
+                GamePlayer aGamePlayer = new GamePlayer(port, gamer);
+                return aGamePlayer;
+            }
+        }
+        return null;
+    }
+
+    private static void prepareMatch() {
+
+        // Play game!
+        // List<String> hostNames = new ArrayList<String>();
+        // List<String> userNames = new ArrayList<®String>();
+        // hostNames.add("localhost");
+        // userNames.add("utv");
+        // portNumbers.add(port);
+        // String gameKey = "hex";
+        // System.out.println("........ Start game!");
+        // playGame(hostNames, userNames, portNumbers, gameKey, aGamePlayer);
+        // System.out.println("........ End game!");
+        // List<Integer> portNumbers = new ArrayList<Integer>();
+    }
+
+    public static void main(String[] args) {
+
+        TournamentManager tm = new TournamentManager("TicTacToe");
+        System.out.println(tm.getTournamentName());
 
         try {
-            
-            URL url = new File(pathToClasses).toURL();
-            URL[] urls = new URL[]{url};
-            ClassLoader cl = new URLClassLoader(urls);
-            String[] extensions = {"class"};
-            String packageName = new File(pathToClasses).listFiles()[0].getName();
-            String pathToPackage = pathToClasses + "/" + packageName;
-            Collection<File> allClassFiles = 
-                FileUtils.listFiles(new File(pathToPackage), extensions, false);
-            
-            // Loop through all class files to find Gamer class.
-            for (Iterator<File> it = allClassFiles.iterator(); it.hasNext();) {
-                File f = it.next();
-                System.out.println("File name = " + f.getName());
-                String playerName = f.getName().split("\\.(?=[^\\.]+$)")[0];
-                String playerPackage = packageName + "." + playerName;
-                Class aClass = cl.loadClass(playerPackage);
-                
-                System.out.println("...........detecting gamer");
-                // found one and update player name, status and path to classes.
-                if (Gamer.class.isAssignableFrom(aClass)) {
-                    System.out.println( f.getName() 
-                        + " is a player!!, and it is " + aClass.getSimpleName());
-                    Gamer gamer = (Gamer) aClass.newInstance();
-                    int port = 9147;
-                    GamePlayer aGamePlayer = new GamePlayer(port, gamer);
-                    aGamePlayer.start();
-                    // aGamePlayer.shutdown();
-                    return;
-                }
-            }
 
-            
+//            List<String> users = tm.usersInTournament();
+//            long seed = System.nanoTime();
+//            Collections.shuffle(users, new Random(seed));
+//            tm.playOneVsOne(users.subList(0, 2));
+//            System.out.println(" Hey ....");
+
+//            List<Document> userNames = new ArrayList<>();
+//            for (int i = 0; i < 3; i++) {
+//                userNames.add(new Document("username", Integer.toString(i)).append("score", 10));
+//            }
+//
+//            Document newRanks = new Document("result", userNames);
+//            System.out.println(newRanks.toJson());
 
         } catch (Exception e) {
             e.printStackTrace();
+            tm.shutdown();
         }
-
-        mongoClient.close();
     }
 }
