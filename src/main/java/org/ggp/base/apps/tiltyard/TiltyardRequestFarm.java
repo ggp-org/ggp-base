a1c05fde1b462c02446174706df9f3fba8ed4d7c
samschreiber <sam.schreiber@gmail.com>
2016-04-09 16:12:49 -0700

    Fix the classpath for ggp-base so the project compiles, and eliminate an unnecessary warning suppression.

--- src/main/java/org/ggp/base/apps/tiltyard/TiltyardRequestFarm.java
+++ src/main/java/org/ggp/base/apps/tiltyard/TiltyardRequestFarm.java
@@ -307,7 +307,6 @@ public final class TiltyardRequestFarm
         }
     }
 
-    @SuppressWarnings("resource")
     public static void main(String[] args) {
         try (ServerSocket listener = new ServerSocket(SERVER_PORT)) {
             if (!testMode) {
